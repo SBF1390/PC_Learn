d0ac7599f6e394e86a5e36e1cee7fb84abb5fda1
SBF1390 <sbf1390@gmail.com>
2025-09-25 06:48:35 +0330

    Installed JWT / Drf Spectacular

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView
 
 urlpatterns = [
-    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
