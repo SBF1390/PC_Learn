c2dbbde3f68b905b6c6328bd97b7ff5f3c984458
SBF1390 <sbf1390@gmail.com>
2025-09-25 07:17:23 +0330

    Models Made For User / Views.py Reqs imported

--- Authentication/models.py
+++ Authentication/models.py
@@ -1,2 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
+
+class UserBase(AbstractBaseUser, PermissionsMixin):
+    FName = models.CharField(max_length=75)
+    LName = models.CharField(max_length=75)
+    Nc = models.CharField(max_length=11, unique=True,)
+
+    USERNAME_FIELD = 'Nc'
+    REQUIRED_FIELDS = ['FName','LName']
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, Nc, password=None, **extra_fields):
+        user = self.model(Nc=Nc, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, Nc, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(Nc, password, **extra_fields)
--- Authentication/views.py
+++ Authentication/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from drf_spectacular.utils import extend_schema, inline_serializer, OpenApiResponse, OpenApiParameter
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import serializers
+from rest_framework.permissions import IsAuthenticated
+from .models import *
 
