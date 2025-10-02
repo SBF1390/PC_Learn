cf8ffee49ca13e4cde358c752755784503e8d3ed
mehrad <mehradahsani4@gmail.com>
2025-10-02 10:46:25 +0330

    Temp App Deleted

--- Blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('blog/' , views.BlogGenericApiView.as_view()),
-    path('blog/<str:name>/' , views.BlogMixinDetailApiView.as_view()),
-    path('blog/<str:name>/<str:author>/' , views.BlogMixinDetailApiView.as_view()),
-]
